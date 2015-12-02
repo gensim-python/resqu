06597e4124a8d943a11dffb707075d75d675b8d2
aksnish <aksnish11@gmail.com>
2015-12-02 17:40:48 -0500

    latest

--- src/dictionaryCreationPipeline.py
+++ src/dictionaryCreationPipeline.py
@@ -5,12 +5,14 @@ import os
 BASEDIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
 # print os.path.abspath(BASEDIR)
 DATADIR = os.path.join(os.path.abspath(BASEDIR), "data")
-# print DATADIR
+print DATADIR
 
 
 # Step 1
 #Loading the existing dictionary
-dictionary = corpora.Dictionary.load("abstracts_without_GS.dict")
+DICTDIR = os.path.join(DATADIR,"abstracts_without_GS.dict")
+print DICTDIR
+dictionary = corpora.Dictionary.load(DICTDIR)
 # #add the gold standard tokens to the existing corpus dictionary
 GSDATADIR = os.path.join(DATADIR,"abstracts_with_GS_rawText.txt")
 for line in open(GSDATADIR, "r"):
@@ -22,22 +24,32 @@ for i in range(len(splitLine)):
     text.insert(splitLine[i])
     dictionary.add_documents(list(text))
 
-# Step 2
-# corpus = list() #list of list each list is a bag of words for a document
-# for concept in summary:
-# 	pmids = get_pmids_for_concept(concept)
-# 	concept_context = list()
-# 	for pmid in pmids:
-# 		context = get_text_as_list(pmid)
-# 		concept_context.append(context)
-# 	#convert the list of concept token to bag of words using the dictionary
-# 	concept_bow = dictionary.doc2bow(concept_context)
-#
-# 	#add the bag of words for the concept to the corpus
-# 	corpus.append(concept_bow)
-#
-# #generate the tfidf model from the corpus (list of lists)
-# tfidf = TfidfModel(corpus)
+
+
+
+def get_pmids_for_concept(concept):
+    text
+
+
+def get_text_as_list(pmid):
+    text
+
 
 
 # Step 2
+summary = ["Migraine Disorders","Diabetes","Myocardial Infarction"]
+corpus = list() #list of list each list is a bag of words for a document
+for concept in summary:
+	pmids = get_pmids_for_concept(concept)
+	concept_context = list()
+	for pmid in pmids:
+		context = get_text_as_list(pmid)
+		concept_context.append(context)
+	#convert the list of concept token to bag of words using the dictionary
+	concept_bow = dictionary.doc2bow(concept_context)
+
+	#add the bag of words for the concept to the corpus
+	corpus.append(concept_bow)
+
+#generate the tfidf model from the corpus (list of lists)
+tfidf = models.TfidfModel(corpus)
