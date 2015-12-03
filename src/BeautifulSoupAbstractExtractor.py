7eca9adc0366b3426b662022e40f50d555866e06
aksnish <aksnish11@gmail.com>
2015-12-02 19:58:44 -0500

    commit

--- src/BeautifulSoupAbstractExtractor.py
+++ src/BeautifulSoupAbstractExtractor.py
@@ -3,17 +3,32 @@ import os
 import subprocess
 
 #base directory path
-BASEDIR="/home/nishita/resqu/data/documentCorpus/"
+# BASEDIR="/home/nishita/resqu/data/documentCorpus/"
+BASEDIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
+pmids_file = os.path.join(BASEDIR, 'data', 'documentCorpus/')
+print(pmids_file)
 #BASEDIR = os.path.join(os.path.dirname(__file__), os.path.pardir)
 
 
-for file in os.listdir("data"):
+for file in os.listdir(pmids_file):
      print(file)
-     filepath = os.path.join(BASEDIR, file)
+     filepath = os.path.join(pmids_file, file)
      print(filepath)
      soup = BeautifulSoup(open(filepath),"html.parser")
 
-     for strong_tag in soup.find_all('h3'):
-          if strong_tag.text == 'Abstract' :
-              print strong_tag.next_sibling.text
-              break
+     # for strong_tag in soup.find_all('h3'):
+     #      if strong_tag.text == 'Abstract' :
+     #          print strong_tag.next_sibling.text
+     #          break
+
+     result = soup.findAll("a", {"title": "National Library of Medicine"})
+
+     for results in result :
+         print(result)
+
+
+
+     # for strong_tag in soup.findAll('a', {'alterm' : 'Radiology'}):
+     #     # if strong_tag.text == 'Abstract' :
+     #     print strong_tag.next_sibling.text
+     #     break
